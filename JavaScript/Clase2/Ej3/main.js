0db62a02b9fdc5bc387185a30d84d2bdba393f6c
CorradiGonzalo <corradigonzalo9@gmail.com>
2025-07-23 21:43:23 -0300

    Second Commit

---
+++ JavaScript/Clase2/Ej3/main.js
@@ -0,0 +1,8 @@
+console.log("Operadores Logicos");
+
+let tieneDinero = true;
+let productoDisponible = true;
+let tiendaAbierta = false;
+
+let puedeComprar = tieneDinero && productoDisponible && tiendaAbierta;
+console.log("El ususario puede comprar? ", puedeComprar);
