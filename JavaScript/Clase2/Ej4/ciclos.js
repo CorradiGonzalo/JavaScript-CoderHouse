85f4e2df092d7d136c267f3cf2b9e047fb9cf479
CorradiGonzalo <corradigonzalo9@gmail.com>
2025-07-23 22:40:38 -0300

    four commit

--- JavaScript/Clase2/Ej4/ciclos.js
+++ JavaScript/Clase2/Ej4/ciclos.js
@@ -1,3 +1,32 @@
 for (let i = 1; i <= 5 ; i ++){
     console.log("vuelta numero; ", i);
 }
+
+let contador = 0;
+while(contador <= 5){
+    console.log("contador");
+    contador ++
+}
+
+let contador2 = 6;
+do{
+    console.log("contador", contador2);
+} while(contador2 <= 5);
+
+let numero1 = 10;
+let numero2 = 5;
+let operacion = prompt("Ingrese una operacion: ")
+switch(operacion){
+    case "+":
+        console.log("resultado: ", numero1 + numero2);
+        break
+    case "-":
+        console.log("resultado: ", numero1 + numero2);
+        break
+    case "*":
+        console.log("resultado: ", numero1 * numero2);
+        break
+    case "/":
+        console.log("resultado: ", numero1/numero2);
+        break
+}
